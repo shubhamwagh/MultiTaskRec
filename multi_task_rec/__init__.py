9d0ba031cdad5a2e3f6fb4b59b48ac6bfaa556d6
shubham <shubham@Dell-G5-5587>
2023-04-04 02:06:21 +0100

    added scripts for dataset, evaluation ,losses and utils

---
+++ multi_task_rec/__init__.py
@@ -0,0 +1,4 @@
+from multi_task_rec.dataset import get_movielens_dataset
+from multi_task_rec.evaluation import mrr_score, mse_score
+from multi_task_rec.models import MultiTaskNet
+from multi_task_rec.multitask import MultitaskModel
